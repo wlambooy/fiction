9f0f1aead308a93c2493b9b38ce6f5d56e583099
wlambooy <willem.lambooy@gmail.com>
2025-11-13 12:26:58 +0000

    :zap: longer global pruning

--- include/fiction/algorithms/physical_design/advanced_circuit_design.hpp
+++ include/fiction/algorithms/physical_design/advanced_circuit_design.hpp
@@ -1156,7 +1156,7 @@ class advanced_circuit_design_impl
                 gate_fitness_assessments.at(gate_index).selected = true;
             }
 
-            if ((global_pruning && first_passing_ix == gate_fitness_assessments.size() - 1) ||
+            if ((global_pruning && first_passing_ix == 0) ||
                 (!global_pruning &&
                  static_cast<double>(first_passing_ix) / static_cast<double>(gate_fitness_assessments.size()) <
                      (1.0 - params.selectivity_tolerance) * selectivity))
